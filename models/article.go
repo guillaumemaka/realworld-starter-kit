e5bb476bb71440d7fe5747ee43cbc8980bb4fa7c
Guillaume Maka <guillaume.maka@gmail.com>
2017-05-13 17:09:03 -0400

    Fix test Bearer -> Token + refactoring

--- models/article.go
+++ models/article.go
@@ -1,7 +1,12 @@
 package models
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
+	"net/url"
+	"reflect"
+	"strconv"
 	"time"
 
 	"github.com/Machiel/slugify"
@@ -12,16 +17,21 @@ type ArticleStorer interface {
 	CreateArticle(*Article) error
 	DeleteArticle(*Article) error
 	GetAllArticles() *gorm.DB
-	GetAllArticlesAuthoredBy(string) ([]Article, error)
-	GetAllArticlesFavoritedBy(string) ([]Article, error)
-	GetAllArticlesWithTag(string) ([]Article, error)
+	GetAllArticlesAuthoredBy(string, int, int) ([]Article, error)
+	GetAllArticlesFavoritedBy(string, int, int) ([]Article, error)
+	GetAllArticlesWithTag(string, int, int) ([]Article, error)
 	GetArticle(string) (*Article, error)
-	FavoriteArticle(int, int) error
-	UnfavoriteArticle(int, int) error
+	FavoriteArticle(*User, *Article) error
+	UnfavoriteArticle(*User, *Article) error
 	FindUserByUsername(string) (*User, error)
 	IsFavorited(int, int) bool
 	IsFollowing(int, int) bool
 	SaveArticle(*Article) error
+	FilterAuthoredBy(*gorm.DB, interface{}) *gorm.DB
+	FilterFavoritedBy(*gorm.DB, interface{}) *gorm.DB
+	FilterByTag(*gorm.DB, interface{}) *gorm.DB
+	Limit(*gorm.DB, interface{}) *gorm.DB
+	Offset(*gorm.DB, interface{}) *gorm.DB
 }
 
 // Article the article model
@@ -41,6 +51,16 @@ type Article struct {
 }
 
 type ValidationMessages map[string]interface{}
+
+var (
+	errorArticleAlreadyFavorited = errors.New("This article is already in your favorites !")
+	errorArticleNotFavorited     = errors.New("Cannot remove this article from your favorites. This article is not in your favorites !")
+)
+
+const (
+	defaultOffset = 0
+	defaultLimit  = 20
+)
 
 // NewArticle returns a new Article instance.
 func NewArticle(title string, description string, body string, user *User) *Article {
@@ -86,13 +106,15 @@ func (db *DB) CreateArticle(article *Article) (err error) {
 	return
 }
 
-// DeleteArticle persist a new article
+// DeleteArticle delete an article
+// WARNING You must provide a primary key in the struct orherwise
+// gorm will delete all article models.
 func (db *DB) DeleteArticle(article *Article) (err error) {
 	err = db.Delete(&article).Error
 	return
 }
 
-// SaveArticle save an article to the database.
+// SaveArticle save/update an article to the database.
 func (db *DB) SaveArticle(article *Article) (err error) {
 	err = db.Save(&article).Error
 	return
@@ -101,41 +123,44 @@ func (db *DB) SaveArticle(article *Article) (err error) {
 // GetArticle retrieve an article by it slug
 func (db *DB) GetArticle(slug string) (*Article, error) {
 	var article Article
-	err := db.DB.Scopes(defaultScope).First(&article, "slug = ?", slug).Error
+	err := db.DB.Scopes(defaultArticleScope).First(&article, "slug = ?", slug).Error
 	return &article, err
 }
 
-// GetAllArticles returns all articles.
+// GetAllArticles return a scope query to fetch all articles.
+// You must call Find at the end to perform the query.
 func (db *DB) GetAllArticles() *gorm.DB {
-	return db.Scopes(defaultScope)
+	return db.Scopes(defaultArticleScope)
 }
 
-func (db *DB) GetAllArticlesWithTag(tagName string) (articles []Article, err error) {
-	err = db.Scopes(defaultScope).
-		Joins("JOIN taggings ON taggings.article_id = articles.id").
-		Joins("JOIN tags ON tags.id = taggings.tag_id").
-		Where("tags.name = ?", tagName).
-		Find(&articles).Error
+// GetAllArticlesWithTag get all articles containings the given tag name.
+func (db *DB) GetAllArticlesWithTag(tagName string, limit int, offset int) (articles []Article, err error) {
+	scopedQuery := db.FilterByTag(db.Scopes(defaultArticleScope), tagName)
+	scopedQuery = db.Limit(scopedQuery, limit)
+	scopedQuery = db.Offset(scopedQuery, offset)
+	err = scopedQuery.Find(&articles).Error
 	return
 }
 
-func (db *DB) GetAllArticlesAuthoredBy(username string) (articles []Article, err error) {
-	err = db.Scopes(defaultScope).
-		Joins("JOIN users ON users.id = articles.user_id").
-		Where("users.username = ?", username).
-		Find(&articles).Error
+// GetAllArticlesAuthoredBy get all articles authored by the given username
+func (db *DB) GetAllArticlesAuthoredBy(username string, limit int, offset int) (articles []Article, err error) {
+	scopedQuery := db.FilterAuthoredBy(db.Scopes(defaultArticleScope), username)
+	scopedQuery = db.Limit(scopedQuery, limit)
+	scopedQuery = db.Offset(scopedQuery, offset)
+	err = scopedQuery.Find(&articles).Error
 	return
 }
 
-func (db *DB) GetAllArticlesFavoritedBy(username string) (articles []Article, err error) {
-	err = db.Scopes(defaultScope).
-		Joins("JOIN favorites ON favorites.article_id = articles.id").
-		Joins("JOIN users ON users.id = favorites.user_id").
-		Where("users.username = ?", username).
-		Find(&articles).Error
+// GetAllArticlesFavoritedBy get all article favorited by the given username
+func (db *DB) GetAllArticlesFavoritedBy(username string, limit int, offset int) (articles []Article, err error) {
+	scopedQuery := db.FilterFavoritedBy(db.Scopes(defaultArticleScope), username)
+	scopedQuery = db.Limit(scopedQuery, limit)
+	scopedQuery = db.Offset(scopedQuery, offset)
+	err = scopedQuery.Find(&articles).Error
 	return
 }
 
+// IsFavorited check if the given user ID favorited the given article ID
 func (db *DB) IsFavorited(userID int, articleID int) bool {
 	f := Favorite{ArticleID: articleID, UserID: userID}
 	if db.Where(f).First(&f).RecordNotFound() {
@@ -144,38 +169,47 @@ func (db *DB) IsFavorited(userID int, articleID int) bool {
 	return true
 }
 
+// IsFollowing check if the given userIDFrom follows userIDTo
 func (db *DB) IsFollowing(userIDFrom int, userIDTo int) bool {
 	// TODO
 	return false
 }
 
+// FindUserByUsername find a user by its username
 func (db *DB) FindUserByUsername(username string) (*User, error) {
 	var user User
 	err := db.First(&user, "username = ?", username).Error
 	return &user, err
 }
 
-func (db *DB) FavoriteArticle(userID int, articleID int) error {
+// FavoriteArticle add the article to the favorites of the given user
+func (db *DB) FavoriteArticle(u *User, a *Article) error {
 	var err error
-	f := Favorite{UserID: userID, ArticleID: articleID}
+	f := Favorite{UserID: u.ID, ArticleID: a.ID}
 
-	if !db.IsFavorited(userID, articleID) {
-		err = db.Create(&f).Error
+	if !db.IsFavorited(u.ID, a.ID) {
+		err = db.Model(&a).Association("Favorites").Append(f).Error
 	} else {
-		err = fmt.Errorf("This article is already in your favorites !")
+		err = errorArticleAlreadyFavorited
 	}
 
 	return err
 }
 
-func (db *DB) UnfavoriteArticle(userID int, articleID int) error {
+// UnfavoriteArticle remove the article to the favorites of the given user
+func (db *DB) UnfavoriteArticle(u *User, a *Article) error {
 	var err error
-	f := Favorite{UserID: userID, ArticleID: articleID}
+	f := Favorite{}
 
-	if db.IsFavorited(userID, articleID) {
+	if !db.First(&f, Favorite{UserID: u.ID, ArticleID: a.ID}).RecordNotFound() {
+		// We can't use the association mode because AfterCreate callbacks on favorite
+		// will not be called and favorties_count will not be decremented
+		// so we need to use Delete() directly.
 		err = db.Delete(&f).Error
+		// Reload the article reference, to update the favorites_count
+		err = db.First(&a).Error
 	} else {
-		err = fmt.Errorf("Cannot remove this article from your favorites. This article is not in your favorites !")
+		err = errorArticleNotFavorited
 	}
 
 	return err
@@ -184,21 +218,208 @@ func (db *DB) UnfavoriteArticle(userID int, articleID int) error {
 // Callbacks
 
 // BeforeCreate gorm callback
+// Titile slugyfication
 func (a *Article) BeforeCreate() (err error) {
 	a.Slug = slugify.Slugify(a.Title)
 	return
 }
 
+// BeforeUpdate gorm callback
+// Titile slugyfication
 func (a *Article) BeforeUpdate() (err error) {
 	a.Slug = slugify.Slugify(a.Title)
 	return
 }
 
-// Scopes
+// FilterByTag filtering article by tag name
+func (DB) FilterByTag(db *gorm.DB, value interface{}) *gorm.DB {
+	var whereClause string
+	var args interface{}
+	var skip = true
+
+	switch value.(type) {
+	case url.Values:
+		whereClause, args, skip = buildWhereClause("tags.name", "tag", value)
+	default:
+		whereClause, args, skip = buildWhereClause("tags.name", value)
+	}
+
+	if !skip {
+		return db.Joins("JOIN taggings ON taggings.article_id = articles.id").
+			Joins("JOIN tags ON tags.id = taggings.tag_id").
+			Where(whereClause, args)
+	}
+
+	return db
+}
+
+// FilterAuthoredBy filtering article by author username
+func (DB) FilterAuthoredBy(db *gorm.DB, value interface{}) *gorm.DB {
+	var whereClause string
+	var args interface{}
+	var skip = true
+
+	switch value.(type) {
+	case url.Values:
+		whereClause, args, skip = buildWhereClause("users.username", "author", value)
+	default:
+		whereClause, args, skip = buildWhereClause("users.username", value)
+	}
+
+	if !skip {
+		return db.Joins("JOIN users ON users.id = articles.user_id").
+			Where(whereClause, args)
+	}
+
+	return db
+}
+
+// FilterFavoritedBy filter articles favorited by user(s) username, value argument can be string|[]string|url.Values
+// If a url.Values provided, it must contains a query string 'favorited' param name.
+func (DB) FilterFavoritedBy(db *gorm.DB, value interface{}) *gorm.DB {
+	var whereClause string
+	var args interface{}
+	var skip = true
+
+	switch value.(type) {
+	case url.Values:
+		whereClause, args, skip = buildWhereClause("users.username", "favorited", value)
+	default:
+		whereClause, args, skip = buildWhereClause("users.username", value)
+	}
+
+	if !skip {
+		var ids []int
+		err := db.New().
+			Model(&User{}).
+			Where(whereClause, args).
+			Pluck("id", &ids).Error
+
+		if err != nil {
+			return db
+		}
+
+		return db.Joins("JOIN favorites ON favorites.article_id = articles.id").
+			Where("favorites.user_id IN (?)", ids)
+	}
+
+	return db
+}
+
+// Offset set the wanted offset (defaulf: 0) to an existing *gorm.DB instance.
+func (DB) Offset(db *gorm.DB, offset interface{}) *gorm.DB {
+	var offsetValue = offset
+
+	switch offset.(type) {
+	case url.Values:
+		queryParams := offset.(url.Values)
+		v := queryParams.Get("offset")
+
+		if v != "" {
+			intVal, err := strconv.Atoi(v)
+			if err != nil {
+				offsetValue = defaultOffset
+			} else {
+				offsetValue = intVal
+			}
+		} else {
+			offsetValue = defaultOffset
+		}
+	}
+
+	return db.Offset(offsetValue)
+}
+
+// Limit set the number of max articles to fetch (defaulf: 20) to an existing *gorm.DB instance.
+func (DB) Limit(db *gorm.DB, limit interface{}) *gorm.DB {
+	var limitValue = limit
+
+	switch limit.(type) {
+	case url.Values:
+		queryParams := limit.(url.Values)
+		v := queryParams.Get("limit")
+
+		if v != "" {
+			intVal, err := strconv.Atoi(v)
+			if err != nil {
+				limitValue = defaultLimit
+			} else {
+				limitValue = intVal
+			}
+		} else {
+			limitValue = defaultLimit
+		}
+	}
+
+	return db.Limit(limitValue)
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Scopes															 		 //
+///////////////////////////////////////////////////////////////////////////////
 
 // Order articles by created_at DESC eager loading Tags and User
-func defaultScope(db *gorm.DB) *gorm.DB {
+func defaultArticleScope(db *gorm.DB) *gorm.DB {
 	return db.Order("articles.created_at desc").
 		Preload("Tags").
 		Preload("User")
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Private Methods															 //
+///////////////////////////////////////////////////////////////////////////////
+
+func buildWhereClause(field string, vars ...interface{}) (string, interface{}, bool) {
+	var whereClause, key string
+	var args, value interface{}
+	// skip determine if we apply or not the query
+	// case for a string true if the value is empty
+	// case for Slice true if slice length is equal to 0
+	// case for a url.Values (map) if the value.Get(key) return an empty string
+	var skip bool
+
+	if len(vars) == 1 {
+		// Case with primitive
+		// buildWhereClause(field, value)
+		value = vars[0]
+	} else if len(vars) == 2 {
+		// Case for an url.Values
+		// buildWhereClause(field, key, value)
+		key, value = vars[0].(string), vars[1]
+	}
+
+	switch value.(type) {
+	case string:
+		skip = (value.(string) == "")
+		if !skip {
+			whereClause = "%v = ?"
+			args = value
+		}
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, sql.NullInt64:
+		whereClause = "%v = ?"
+		args = value
+	case []int, []int8, []int16, []int32, []int64, []uint, []uint8, []uint16, []uint32, []uint64, []string, []interface{}:
+		skip = reflect.ValueOf(value).Len() == 0
+		if !skip {
+			whereClause = "%v IN (?)"
+			args = value
+		}
+	case url.Values:
+		queryParams := value.(url.Values)
+		v := queryParams.Get(key)
+		skip = (v == "")
+		if !skip {
+			whereClause = "%v = ?"
+			args = v
+		}
+	default:
+		skip = true
+	}
+
+	if skip {
+		return "", nil, skip
+	}
+
+	return fmt.Sprintf(whereClause, field), args, skip
+
 }
--- models/favorite.go
+++ models/favorite.go
@@ -1,9 +1,23 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type Favorite struct {
 	ID        int
 	User      User
 	UserID    int
 	Article   Article
 	ArticleID int
+}
+
+func (f *Favorite) AfterCreate(db *gorm.DB) (err error) {
+	var a = &Article{ID: f.ArticleID}
+	err = db.First(&a).Update("favorites_count", gorm.Expr("favorites_count + ?", 1)).Error
+	return
+}
+
+func (f *Favorite) AfterDelete(db *gorm.DB) (err error) {
+	var a = &Article{ID: f.ArticleID}
+	err = db.First(&a).Update("favorites_count", gorm.Expr("favorites_count - ?", 1)).Error
+	return
 }
